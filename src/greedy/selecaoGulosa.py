f3778506a42120cce714620112c863e756ac9584
Snorlaxch <zyroch6@gmail.com>
2026-05-31 18:58:35 -0300

    Teste de algoritmo guloso funcionando com uma lista de atividades

--- src/greedy/selecaoGulosa.py
+++ src/greedy/selecaoGulosa.py
@@ -1,6 +1,74 @@
-from src.models.Atividade import Atividade
+from datetime import time
+from src.models.atividade import Atividade
+from src.utils.prioridade import Prioridade
 
-def guloso(lista= list()):
+from src.sorting.MergeSort import merge_sort as msort
+import random
+
+
+def guloso(atividades, ordenacao= "fim"):
+
+    if ordenacao == "prioridade":
+        chave= lambda a: a.getPrioridade()
+    elif ordenacao == "inicio":
+        chave= lambda a: a.getHorarioInicio()
+    elif ordenacao == "fim":
+        chave= lambda a: a.getHorarioFim()
     
-    for i in range(len(lista)-1):
-        pass
+    ordenadas = msort(atividades, chave )
+
+    selecionadas = []
+
+    ultima = None
+
+    for atividade in ordenadas:
+
+        if ultima is None:
+            selecionadas.append(atividade)
+
+            ultima= atividade
+
+        elif atividade.getHorarioInicio() >= ultima.getHorarioFim():
+
+            selecionadas.append(atividade)
+            ultima = atividade
+
+    return selecionadas
+        
+
+if __name__ == "__main__":
+    
+    def gerar_lista_atividades(numero):
+
+        atividades= []
+
+        for i in range(numero):
+            prio= random.randint(1,3)
+            inicio= random.randint(1,19)
+            fim= inicio + random.randint(1,2)
+
+            nomes=[ "Reunião de Equipe", "Reunião com Cliente", "Treinamento Interno", "Entrevista de Candidato", "Planejamento Estratégico", "Apresentação de Resultados", "Workshop de Inovação", "Revisão de Projeto"]
+            nome_atv= random.choice(nomes)
+            participantes= random.randint(1,20)
+
+            atividades.append(Atividade(nome_atv, time(inicio, 0), time(fim, 0), Prioridade(prio), participantes))
+
+        return atividades
+
+
+    lista= gerar_lista_atividades(10)
+
+    print(len(lista))
+    for atv in lista:
+        print(atv)
+        print()
+    print("####################################################################")
+    
+    lista= guloso(lista, "fim")
+
+
+    print(len(lista))
+
+    for atv in lista:
+        print(atv.getNome())
+        print()
--- src/tests/sortingTest/SortAtividadeTest.py
+++ src/tests/sortingTest/SortAtividadeTest.py
@@ -5,10 +5,5 @@ from src.utils.prioridade import Prioridade
 
 
 def gerar_lista_atividades(numero):
-    
-    atividades= []
-    for i in range(numero):
-        prio= 
-        atividades.append(Atividade("Reunião A", time(10, 0), time(12, 0), Prioridade.ALTA, 5))
-            pass
+    pass
 
