80275ad4353cb457766326b702808076e6307171
Snorlaxch <zyroch6@gmail.com>
2026-05-31 18:04:23 -0300

    'Criando teste do Guloso'

--- src/greedy/selecao_gulosa.py
+++ src/greedy/selecao_gulosa.py
@@ -1,6 +1,8 @@
 from src.models.atividade import Atividade as at
 from datetime import time
 from sorting.MergeSort import merge_sort as msort
+import random
+
 
 def guloso(atividades:list, ordenacao= "fim"):
 
@@ -31,11 +33,21 @@ def guloso(atividades:list, ordenacao= "fim"):
 
     return selecionadas
         
-    
-
-
-
 
 if __name__ == "__main__":
+    from datetime import time
+    from src.sorting.MergeSort import merge_sort
+    from src.models.atividade import Atividade
+    from src.utils.prioridade import Prioridade
 
+
+    def gerar_lista_atividades(numero):
+        atividades= []
+
+        for i in range(numero):
+            prio= random.randint(1,3)
+            atividades.append(Atividade("Reunião A", time(10, 0), time(12, 0), Prioridade(prio), 5))
+            pass
+
+    print(Prioridade(1))
 
---
+++ src/tests/sortingTest/SortAtividadeTest.py
@@ -0,0 +1,14 @@
+from datetime import time
+from src.sorting.MergeSort import merge_sort
+from src.models.atividade import Atividade
+from src.utils.prioridade import Prioridade
+
+
+def gerar_lista_atividades(numero):
+    
+    atividades= []
+    for i in range(numero):
+        prio= 
+        atividades.append(Atividade("Reunião A", time(10, 0), time(12, 0), Prioridade.ALTA, 5))
+            pass
+
