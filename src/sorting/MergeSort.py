b0e1037ca3211e54b488f5ab60eb87e8965a196c
Nicole Silva <nicolesilva8144@gmail.com>
2026-06-02 19:31:31 -0300

    Adicionando comentários na classe do mergeSORT

--- src/sorting/MergeSort.py
+++ src/sorting/MergeSort.py
@@ -1,14 +1,15 @@
 def merge_sort(lista, chave=lambda x: x):
+    """
+    Implementação  do Merge Sort para ordenação de atividades.
+    """
     if len(lista) <= 1:
         return lista
 
     meio = len(lista) // 2
-
     esquerda = merge_sort(lista[:meio], chave)
     direita = merge_sort(lista[meio:], chave)
 
     return merge(esquerda, direita, chave)
-
 
 def merge(esquerda, direita, chave):
     resultado = []
@@ -16,6 +17,7 @@ def merge(esquerda, direita, chave):
     j = 0
 
     while i < len(esquerda) and j < len(direita):
+        # Compara usando a chave fornecida (horário, prioridade, etc)
         if chave(esquerda[i]) <= chave(direita[j]):
             resultado.append(esquerda[i])
             i += 1
@@ -25,5 +27,5 @@ def merge(esquerda, direita, chave):
 
     resultado.extend(esquerda[i:])
     resultado.extend(direita[j:])
-
+    
     return resultado
